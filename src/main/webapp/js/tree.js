6cd2221e8b87a23d0e55e301c0e875f36f952275
alexandr <santoson17@gmail.com>
2016-04-18 19:26:40 +0300

    reading xml file and tree visualization added

---
+++ src/main/webapp/js/tree.js
@@ -0,0 +1,60 @@
+/**
+ * Created by santos on 4/18/16.
+ */
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "resources/tree.xml",
+        dataType: "xml",
+        success: function (xml) {
+            $('#tree').find('ul').remove();
+            $('<ul></ul>').addClass('tree non-terminal').addClass('active').appendTo('#tree');
+            $('.tree').html('<span>Tree</span>');
+            $(xml).children('non-terminal').each(function () {
+                parser($(this));
+                $('.active').removeClass('active');
+                $('.catalog').addClass('active');
+            });
+            $('.tree').children('span:first').off();
+
+            $('.active').removeClass('active');
+            $("ul").addClass('list-group');
+            $("li").addClass('list-group-item');
+        }
+    });
+
+    $(function() {
+        $(window).scroll(function() {
+            var top = $(this).scrollTop();
+            $('#buttons').css('margin-top', top + 10);
+        });
+    });
+});
+
+function parser(dir) {
+    $('<li></li>').addClass('tmp').appendTo('.active');
+    $('.tmp').html(
+        '<span>'+$(dir).attr('name')+'</span>'+'<ul></ul>'
+    );
+    $('.active').removeClass('active');
+    $('.tmp ul').addClass('active');
+    $('.tmp').addClass('non-terminal').removeClass('tmp');
+
+    $("span").off().on("click", function() {
+        $(this).parent('.non-terminal').find("ul").children("li").toggle();
+        return false;
+    });
+
+    $(dir).children('terminal').each(function() {
+        $('<li id='+$(this).text()+'></li><br/>').html($(this).attr('name'))
+            .addClass('terminal').appendTo('.active');
+    });
+    if ($(dir).children('non-terminal').length > 0) {
+        $(dir).children('non-terminal').each(function() {
+            parser($(this));
+        });
+    }
+    $('.active').removeClass('active').parents("ul:first").addClass('tmp');
+    $('.tmp').removeClass('tmp').addClass('active');
+}
+
---
+++ src/main/webapp/tree.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: santos
+  Date: 4/18/16
+  Time: 10:52 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link type="text/css" media="all" rel="stylesheet" href="css/tree.css" />
+    <link type="text/css" media="all" rel="stylesheet" href="css/bootstrap.css" />
+    <link type="text/css" media="all" rel="stylesheet" href="css/bootstrap.min.css" />
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/tree.js"></script>
+</head>
+<body>
+<section class="my-container">
+    <section id="buttons">
+        <a href="output.jsp"><button class="btn btn-primary" type="button">back to result</button></a>
+    </section>
+    <section id="tree"></section>
+</section>
+</body>
+</html>
